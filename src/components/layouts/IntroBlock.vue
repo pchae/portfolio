5f3f475cdff49906c106b7937006f649a45b0c13
pchae <20016424+pchae@users.noreply.github.com>
2021-02-26 18:18:28 -0500

    init

---
+++ src/components/layouts/IntroBlock.vue
@@ -0,0 +1,61 @@
+<template>
+  <section
+    :id="[sectionID]"
+    :class="[padding, bgColor]"
+  >
+    <div class="container-lg">
+      <div class="temp1">
+        <h2 class="section-headline">
+          {{ headline }}
+        </h2>
+        <h3 class="subtext">
+          Founder and principle designer at <a href="https://saturdays.io">Saturdays.io</a>
+        </h3>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'IntroBlock',
+  props: {
+    headline: {
+      type: String,
+      default: '',
+    },
+    desc: {
+      type: String,
+      default: '',
+    },
+    sectionID: {
+      type: String,
+      default: '',
+    },
+  },
+  data() {
+    return {
+      padding: '',
+      bgColor: '',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.subtext {
+  color: #000;
+}
+
+.temp1 {
+  display: flex;
+  flex-direction: column;
+  height: 20rem;
+  justify-content: flex-end;
+}
+@media only screen and (max-width: 640px) {
+}
+
+</style>
