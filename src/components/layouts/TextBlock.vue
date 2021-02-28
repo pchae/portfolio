569a454ce0148a100c9638fc4274f3648ab743e5
pchae <20016424+pchae@users.noreply.github.com>
2021-02-28 05:12:58 -0500

    init

---
+++ src/components/layouts/TextBlock.vue
@@ -0,0 +1,66 @@
+<template>
+  <section
+    :id="[sectionID]"
+    :class="[padding, bgColor]"
+  >
+    <div class="container-lg">
+      <div class="fcol quote pt5">
+        <h2 class="section-headline">
+          {{ headline }}
+        </h2>
+        <h4 class="subtext">
+          {{ desc }}
+        </h4>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'TextBlock',
+  props: {
+    headline: {
+      type: String,
+      default: '',
+    },
+    desc: {
+      type: String,
+      default: '',
+    },
+    sectionID: {
+      type: String,
+      default: '',
+    },
+  },
+  data() {
+    return {
+      padding: '',
+      bgColor: '',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.section-headline {
+  margin-bottom: 2rem;
+}
+
+.subtext {
+  display: flex;
+  color: #000;
+  font-weight: 400;
+  width: 66%;
+  justify-content: flex-end;
+}
+
+.quote {
+  min-height: 10rem;
+  justify-content: flex-end;
+}
+@media only screen and (max-width: 640px) {
+}
+
+</style>
