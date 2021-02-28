e4695f41b9ea95a650e3e0fdfea56601110c7148
pchae <20016424+pchae@users.noreply.github.com>
2021-02-27 21:37:09 -0500

    init

---
+++ src/components/layouts/SectionTitle.vue
@@ -0,0 +1,61 @@
+<template>
+  <section
+    :id="[sectionID]"
+    :class="[padding, bgColor]"
+  >
+    <div class="container-lg">
+      <div class="temp1">
+        <hr class="temp" />
+        <h4 class="section-headline">
+          {{ title }}
+        </h4>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'SectionTitle',
+  props: {
+    title: {
+      type: String,
+      default: '',
+    },
+    desc: {
+      type: String,
+      default: '',
+    },
+    sectionID: {
+      type: String,
+      default: '',
+    },
+  },
+  data() {
+    return {
+      padding: '',
+      bgColor: '',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.temp1 {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+}
+.section-headline {
+  font-weight: 400;
+}
+hr {
+  margin-bottom: 1.25rem;
+}
+
+@media only screen and (max-width: 640px) {
+}
+
+</style>
