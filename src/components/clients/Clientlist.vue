bdf6a58f4d42df79eaa339cf5f6cdfc7ac5f1ba0
pchae <20016424+pchae@users.noreply.github.com>
2021-02-27 21:36:15 -0500

    Clean up template
    - added sectionID prop

--- src/components/clients/Clientlist.vue
+++ src/components/clients/Clientlist.vue
@@ -1,130 +1,126 @@
 <template>
-  <section :class="[padding, bgColor]">
-    <div class="container-lg content-wrapper">
-      <div class="">
-        <h2 class="section-heading">
-          {{ headline }}
-        </h2>
+  <section
+    :id="[sectionID]"
+    :class="[padding, bgColor]">
+    <div class="container content-wrapper">
+      <div class="client-partners">
+        <!-- Row 1 -->
+        <div class="item-container">
+          <Logo
+            :index="1"
+            img-src="cbc.svg"
+            img-alt="Canadian Broadcasting Company logo"
+          />
+        </div>
+        <div class="item-container">
+          <Logo
+            :index="2"
+            img-src="usatoday.svg"
+            img-alt="USA Today logo"
+          />
+        </div>
+        <div class="item-container">
+          <Logo
+            :index="3"
+            img-src="reuters.svg"
+            img-alt="Reuters logo"
+          />
+        </div>
+        <div class="item-container">
+          <Logo
+            :index="4"
+            img-src="rbc.svg"
+            img-alt="Royal Bank of Canada logo"
+          />
+        </div>
+        <!-- Row 2 -->
+        <div class="item-container">
+          <Logo
+            :index="5"
+            img-src="mashable.svg"
+            img-alt="Mashable logo"
+          />
+        </div>
+        <div class="item-container">
+          <Logo
+            :index="6"
+            img-src="starz.svg"
+            img-alt="Starz logo"
+          />
+        </div>
+        <div class="item-container">
+          <Logo
+            :index="7"
+            img-src="pbs.svg"
+            img-alt="Public Broadcasting Service logo"
+          />
+        </div>
+        <div class="item-container">
+          <Logo
+            :index="8"
+            img-src="vice.svg"
+            img-alt="Vice Media logo"
+          />
+        </div>
 
-        <div class="client-partners">
-          <!-- Row 1 -->
-          <div class="item-container">
-            <Logo
-              :index="1"
-              img-src="cbc.svg"
-              img-alt="Canadian Broadcasting Company logo"
-            />
-          </div>
-          <div class="item-container">
-            <Logo
-              :index="2"
-              img-src="usatoday.svg"
-              img-alt="USA Today logo"
-            />
-          </div>
-          <div class="item-container">
-            <Logo
-              :index="3"
-              img-src="reuters.svg"
-              img-alt="Reuters logo"
-            />
-          </div>
-          <div class="item-container">
-            <Logo
-              :index="4"
-              img-src="rbc.svg"
-              img-alt="Royal Bank of Canada logo"
-            />
-          </div>
-          <!-- Row 2 -->
-          <div class="item-container">
-            <Logo
-              :index="5"
-              img-src="mashable.svg"
-              img-alt="Mashable logo"
-            />
-          </div>
-          <div class="item-container">
-            <Logo
-              :index="6"
-              img-src="starz.svg"
-              img-alt="Starz logo"
-            />
-          </div>
-          <div class="item-container">
-            <Logo
-              :index="7"
-              img-src="pbs.svg"
-              img-alt="Public Broadcasting Service logo"
-            />
-          </div>
-          <div class="item-container">
-            <Logo
-              :index="8"
-              img-src="vice.svg"
-              img-alt="Vice Media logo"
-            />
-          </div>
+        <!-- Row 3 -->
+        <div class="item-container">
+          <Logo
+            :index="9"
+            img-src="ufc.svg"
+            img-alt="Ultimate Fighting Championship logo"
+          />
+        </div>
+        <div class="item-container">
+          <Logo
+            :index="10"
+            img-src="redbull.svg"
+            img-alt="Redbull logo"
+          />
+        </div>
+        <div class="item-container">
+          <Logo
+            :index="11"
+            img-src="latam.svg"
+            img-alt="LATAM Airlines logo"
+          />
+        </div>
+        <div class="item-container">
+          <Logo
+            :index="12"
+            img-src="avianca.svg"
+            img-alt="Avianca Airlines logo"
+          />
+        </div>
 
-          <!-- Row 3 -->
-          <div class="item-container">
-            <Logo
-              :index="9"
-              img-src="ufc.svg"
-              img-alt="Ultimate Fighting Championship logo"
-            />
-          </div>
-          <div class="item-container">
-            <Logo
-              :index="10"
-              img-src="redbull.svg"
-              img-alt="Redbull logo"
-            />
-          </div>
-          <div class="item-container">
-            <Logo
-              :index="11"
-              img-src="latam.svg"
-              img-alt="LATAM Airlines logo"
-            />
-          </div>
-          <div class="item-container">
-            <Logo
-              :index="12"
-              img-src="avianca.svg"
-              img-alt="Avianca Airlines logo"
-            />
-          </div>
-
-          <!-- Row 4 -->
-          <div class="item-container">
-            <Logo
-              :index="13"
-              img-src="twitch.svg"
-              img-alt="Twitch logo"
-            />
-          </div>
-          <div class="item-container">
-            <Logo
-              :index="14"
-              img-src="dell.svg"
-              img-alt="Dell EMC logo"
-            />
-          </div>
-          <div class="item-container">
-            <Logo
-              :index="15"
-              img-src="microsoft.svg"
-              img-alt="Microsoft Corporation logo"
-            />
-          </div>
-          <div class="item-container">
-            <Logo
-              :index="16"
-              img-src="samsung.svg"
-              img-alt="Samsung Electronics logo"
-            />
-          </div>
+        <!-- Row 4 -->
+        <div class="item-container">
+          <Logo
+            :index="13"
+            img-src="twitch.svg"
+            img-alt="Twitch logo"
+          />
+        </div>
+        <div class="item-container">
+          <Logo
+            :index="14"
+            img-src="dell.svg"
+            img-alt="Dell EMC logo"
+          />
+        </div>
+        <div class="item-container">
+          <Logo
+            :index="15"
+            img-src="microsoft.svg"
+            img-alt="Microsoft Corporation logo"
+          />
+        </div>
+        <div class="item-container">
+          <Logo
+            :index="16"
+            img-src="samsung.svg"
+            img-alt="Samsung Electronics logo"
+          />
         </div>
       </div>
     </div>
@@ -141,11 +137,11 @@ export default {
       type: Number,
       default: 1,
     },
-    headline: {
+    imgSrc: {
       type: String,
       default: '',
     },
-    imgSrc: {
+    sectionID: {
       type: String,
       default: '',
     },
@@ -170,7 +166,7 @@ export default {
 .client-partners {
   display: flex;
   flex-wrap: wrap;
-  margin-top: 4rem;
+  margin-top: 0rem;
 }
 
 .item-container {
@@ -180,7 +176,7 @@ export default {
   position: relative;
   width: 25%;
   flex-shrink: 0;
-  padding-right: 6rem;
+  padding-right: 0rem;
   margin-bottom: 6rem;
   cursor: default;
 }
@@ -188,7 +184,7 @@ export default {
 
 @media screen and (min-width: 960px) {
   .client-partners {
-    margin-top: 5rem;
+    margin-top: 0rem;
   }
 }
 
