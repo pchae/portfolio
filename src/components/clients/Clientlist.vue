e93f9a368c70326b41cfb3d66e7a0f5829602586
pchae <20016424+pchae@users.noreply.github.com>
2021-03-02 04:33:20 -0500

    Update  2 column layout

--- src/components/clients/Clientlist.vue
+++ src/components/clients/Clientlist.vue
@@ -1,126 +1,98 @@
 <template>
-  <section
-    :id="[sectionID]"
-    :class="[padding, bgColor]">
-    <div class="container content-wrapper">
-      <div class="client-partners">
-        <!-- Row 1 -->
-        <div class="item-container">
-          <Logo
-            :index="1"
-            img-src="cbc.svg"
-            img-alt="Canadian Broadcasting Company logo"
-          />
+  <section :id="[sectionID]" :class="[padding, bgColor]">
+    <div class="container-lg content-wrapper">
+      <div class="split-two-col">
+        <div class="left-side">
+          <hr class="divider-class" />
+          <h4 class="section-title">Select clients.</h4>
         </div>
-        <div class="item-container">
-          <Logo
-            :index="2"
-            img-src="usatoday.svg"
-            img-alt="USA Today logo"
-          />
-        </div>
-        <div class="item-container">
-          <Logo
-            :index="3"
-            img-src="reuters.svg"
-            img-alt="Reuters logo"
-          />
-        </div>
-        <div class="item-container">
-          <Logo
-            :index="4"
-            img-src="rbc.svg"
-            img-alt="Royal Bank of Canada logo"
-          />
-        </div>
-        <!-- Row 2 -->
-        <div class="item-container">
-          <Logo
-            :index="5"
-            img-src="mashable.svg"
-            img-alt="Mashable logo"
-          />
-        </div>
-        <div class="item-container">
-          <Logo
-            :index="6"
-            img-src="starz.svg"
-            img-alt="Starz logo"
-          />
-        </div>
-        <div class="item-container">
-          <Logo
-            :index="7"
-            img-src="pbs.svg"
-            img-alt="Public Broadcasting Service logo"
-          />
-        </div>
-        <div class="item-container">
-          <Logo
-            :index="8"
-            img-src="vice.svg"
-            img-alt="Vice Media logo"
-          />
-        </div>
+        <div class="client-partners">
+          <!-- Row 1 -->
+          <div class="item-container">
+            <Logo
+              :index="1"
+              img-src="cbc.svg"
+              img-alt="Canadian Broadcasting Company logo"
+            />
+          </div>
+          <div class="item-container">
+            <Logo :index="2" img-src="usatoday.svg" img-alt="USA Today logo" />
+          </div>
+          <div class="item-container">
+            <Logo :index="3" img-src="reuters.svg" img-alt="Reuters logo" />
+          </div>
+          <div class="item-container">
+            <Logo
+              :index="4"
+              img-src="rbc.svg"
+              img-alt="Royal Bank of Canada logo"
+            />
+          </div>
+          <!-- Row 2 -->
+          <div class="item-container">
+            <Logo :index="5" img-src="mashable.svg" img-alt="Mashable logo" />
+          </div>
+          <div class="item-container">
+            <Logo :index="6" img-src="starz.svg" img-alt="Starz logo" />
+          </div>
+          <div class="item-container">
+            <Logo
+              :index="7"
+              img-src="pbs.svg"
+              img-alt="Public Broadcasting Service logo"
+            />
+          </div>
+          <div class="item-container">
+            <Logo :index="8" img-src="vice.svg" img-alt="Vice Media logo" />
+          </div>
 
-        <!-- Row 3 -->
-        <div class="item-container">
-          <Logo
-            :index="9"
-            img-src="ufc.svg"
-            img-alt="Ultimate Fighting Championship logo"
-          />
-        </div>
-        <div class="item-container">
-          <Logo
-            :index="10"
-            img-src="redbull.svg"
-            img-alt="Redbull logo"
-          />
-        </div>
-        <div class="item-container">
-          <Logo
-            :index="11"
-            img-src="latam.svg"
-            img-alt="LATAM Airlines logo"
-          />
-        </div>
-        <div class="item-container">
-          <Logo
-            :index="12"
-            img-src="avianca.svg"
-            img-alt="Avianca Airlines logo"
-          />
-        </div>
+          <!-- Row 3 -->
+          <div class="item-container">
+            <Logo
+              :index="9"
+              img-src="ufc.svg"
+              img-alt="Ultimate Fighting Championship logo"
+            />
+          </div>
+          <div class="item-container">
+            <Logo :index="10" img-src="redbull.svg" img-alt="Redbull logo" />
+          </div>
+          <div class="item-container">
+            <Logo
+              :index="11"
+              img-src="latam.svg"
+              img-alt="LATAM Airlines logo"
+            />
+          </div>
+          <div class="item-container">
+            <Logo
+              :index="12"
+              img-src="avianca.svg"
+              img-alt="Avianca Airlines logo"
+            />
+          </div>
 
-        <!-- Row 4 -->
-        <div class="item-container">
-          <Logo
-            :index="13"
-            img-src="twitch.svg"
-            img-alt="Twitch logo"
-          />
-        </div>
-        <div class="item-container">
-          <Logo
-            :index="14"
-            img-src="dell.svg"
-            img-alt="Dell EMC logo"
-          />
-        </div>
-        <div class="item-container">
-          <Logo
-            :index="15"
-            img-src="microsoft.svg"
-            img-alt="Microsoft Corporation logo"
-          />
-        </div>
-        <div class="item-container">
-          <Logo
-            :index="16"
-            img-src="samsung.svg"
-            img-alt="Samsung Electronics logo"
-          />
+          <!-- Row 4 -->
+          <div class="item-container">
+            <Logo :index="13" img-src="twitch.svg" img-alt="Twitch logo" />
+          </div>
+          <div class="item-container">
+            <Logo :index="14" img-src="dell.svg" img-alt="Dell EMC logo" />
+          </div>
+          <div class="item-container">
+            <Logo
+              :index="15"
+              img-src="microsoft.svg"
+              img-alt="Microsoft Corporation logo"
+            />
+          </div>
+          <div class="item-container">
+            <Logo
+              :index="16"
+              img-src="samsung.svg"
+              img-alt="Samsung Electronics logo"
+            />
+          </div>
         </div>
       </div>
     </div>
@@ -128,10 +100,10 @@
 </template>
 
 <script>
-import Logo from '@/components/clients/Logo.vue';
+import Logo from "@/components/clients/Logo.vue";
 
 export default {
-  name: 'Clientlist',
+  name: "Clientlist",
   props: {
     index: {
       type: Number,
@@ -139,18 +111,18 @@ export default {
     },
     imgSrc: {
       type: String,
-      default: '',
+      default: "",
     },
     sectionID: {
       type: String,
-      default: '',
+      default: "",
     },
   },
 
   data() {
     return {
-      padding: '',
-      bgColor: '',
+      padding: "",
+      bgColor: "",
     };
   },
 
@@ -162,11 +134,30 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.split-two-col {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
+}
+
+.left-side {
+  width: calc(32% - 0.5rem);
+}
+
+hr {
+  color: #fff;
+  width: 200px;
+}
+
+.section-title {
+  color: #fff;
+}
 
 .client-partners {
   display: flex;
   flex-wrap: wrap;
-  margin-top: 0rem;
+  width: calc(64% - 0.5rem);
 }
 
 .item-container {
@@ -176,11 +167,10 @@ export default {
   position: relative;
   width: 25%;
   flex-shrink: 0;
-  padding-right: 0rem;
-  margin-bottom: 6rem;
+  padding-right: 1.25rem;
+  margin-bottom: 5rem;
   cursor: default;
 }
-
 
 @media screen and (min-width: 960px) {
   .client-partners {
@@ -188,14 +178,25 @@ export default {
   }
 }
 
-@media only screen and (max-width: 740px) {
+@media only screen and (max-width: 800px) {
+  .split-two-col {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .left-side {
+    width: 100%;
+  }
+
+  .client-partners {
+    width: 100%;
+    margin-top: 5rem;
+  }
+
   .item-container {
     width: 33.33%;
   }
 
-  .img-grid {
-    grid-template-columns: repeat(3, 1fr);
-  }
 }
 
 @media only screen and (max-width: 640px) {
