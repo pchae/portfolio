0b76b4dedea8e8264e0ac0b93d8c2471acfdac7a
pchae <20016424+pchae@users.noreply.github.com>
2021-03-12 03:39:58 -0500

    init

---
+++ src/components/layouts/text_block.vue
@@ -0,0 +1,74 @@
+<template>
+  <section
+    :id="[sectionID]"
+    :class="[padding, bgColor]"
+  >
+    <div class="container">
+      <div class="fcol pad1">
+        <h3 class="section-headline">
+          {{ intro }}
+        </h3>
+        <div class="frow">
+          <p class="subtext">
+            {{ desc }}
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'text_block',
+  props: {
+    intro: {
+      type: String,
+      default: '',
+    },
+    desc: {
+      type: String,
+      default: '',
+    },
+    sectionID: {
+      type: String,
+      default: '',
+    },
+  },
+  data() {
+    return {
+      padding: '',
+      bgColor: '',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.section-headline {
+}
+
+.frow {
+  justify-content: flex-start;
+}
+
+.subtext {
+  color: #000;
+  font-size: 1.125rem;
+  font-weight: 400;
+}
+
+.frow:nth-child(1n) {
+  margin-bottom: 2rem;
+}
+
+.frow:last-child {
+  margin-bottom: 0rem;
+}
+
+@media only screen and (max-width: 980px) {
+
+}
+
+</style>
