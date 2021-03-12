09d657dea66750b767c9614de49964ea7bb8d242
pchae <20016424+pchae@users.noreply.github.com>
2021-03-12 03:00:45 -0500

    init

---
+++ src/components/layouts/ProjectTitle.vue
@@ -0,0 +1,67 @@
+<template>
+  <section :id="[sectionID]" :class="[padding, bgColor]">
+    <div class="container">
+      <div class="fcol intro pt5">
+        <h2 class="project-headline">
+          {{ project }}
+        </h2>
+        <h4 class="details">
+          {{ role }} • {{ date }}
+        </h4>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "ProjectTitle",
+  props: {
+    sectionID: {
+      type: String,
+      default: "",
+    },
+    project: {
+      type: String,
+      default: "",
+    },
+    role: {
+      type: String,
+      default: "",
+    },
+    date: {
+      type: String,
+      default: "",
+    },
+  },
+  data() {
+    return {
+      padding: "",
+      bgColor: "",
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.details {
+  color: #000;
+  font-weight: 400;
+}
+
+.intro {
+  min-height: 10rem;
+  justify-content: flex-end;
+}
+
+.project-headline {
+  font-size: 5rem;
+}
+
+@media only screen and (max-width: 800px) {
+  .project-headline {
+    font-size: 2.5rem;
+  }
+}
+</style>
