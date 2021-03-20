376e3e3c5a23e9ffac330ed3f0d8cf39886693a3
pchae <20016424+pchae@users.noreply.github.com>
2021-03-20 07:26:55 -0400

    Added inject data

--- src/components/layouts/article_details.vue
+++ src/components/layouts/article_details.vue
@@ -5,26 +5,26 @@
         <div class="list-container">
           <ul>
             <li class="detail-heading">CLIENT</li>
-            <li class="">{{ client }}</li>
+            <li class="">{{ workInfo.client }}</li>
           </ul>
         </div>
         <div class="list-container">
           <ul>
             <li class="detail-heading">ROLE</li>
-            <li class="">{{ role }}</li>
+            <li class="">{{ workInfo.role }}</li>
           </ul>
         </div>
         <div class="list-container">
           <ul>
             <li class="detail-heading">YEAR</li>
-            <li class="">{{ year }}</li>
+            <li class="">{{ workInfo.year }}</li>
           </ul>
         </div>
         <div class="results">
           <div class="list-container-long">
             <ul>
               <li class="detail-heading">RESULTS</li>
-              <li class="">{{ result }}</li>
+              <li v-for="item in workInfo.result" :key="item" class="resultlist">{{ item }}</li>
             </ul>
           </div>
         </div>
@@ -41,22 +41,6 @@ export default {
       type: String,
       default: "",
     },
-    client: {
-      type: String,
-      default: "",
-    },
-    role: {
-      type: String,
-      default: "",
-    },
-    result: {
-      type: String,
-      default: "",
-    },
-    year: {
-      type: String,
-      default: "",
-    },
   },
 
   data() {
@@ -65,8 +49,10 @@ export default {
       bgColor: "",
     };
   },
-
-  components: {},
+  inject: ['workInfo'],
+  created () {
+    console.log(this.workInfo)
+  },
 };
 </script>
 
@@ -82,14 +68,18 @@ export default {
 
 .flex-name1 {
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   flex-wrap: wrap;
-  gap: 0.5rem;
+  gap: 2rem;
   width: 100%;
 }
 
 .results {
   padding-top: 2rem;
+}
+
+.resultlist {
+  padding-bottom: 0.5rem;
 }
 
 li {
