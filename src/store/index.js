0e3eb7de47fa166da23019357cecc0e73de9c062
ajay198810 <56945687+ajay198810@users.noreply.github.com>
2021-04-28 22:16:01 -0400

    ur welcome

--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,12 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import servicelist from '@/json/services.json';
+import { data } from '@/json/services.json';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    services: servicelist,
-  },
-
-  getters: {
-    getServices (state) {
-      return state.services
-    }
+    services: data,
   },
   mutations: {
   },
