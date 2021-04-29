16f88daba2581789cc20b01e13ed788be292f526
ajay198810 <56945687+ajay198810@users.noreply.github.com>
2021-04-28 23:18:02 -0400

    mystic mac for real this fookin time

--- src/components/layouts/text_paragraph.vue
+++ src/components/layouts/text_paragraph.vue
@@ -11,18 +11,13 @@
 </template>
 
 <script>
-import { mapState } from "vuex";
-
+import { about } from "../../json/about.json";
 export default {
   name: "TextPG",
-
-  computed: mapState({
-    about: (state) => state.about,
-  }),
-
   props: {},
   data() {
     return {
+      about,
       padding: "",
       bgColor: "",
       id: "",
