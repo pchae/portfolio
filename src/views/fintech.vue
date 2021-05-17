b4089682398e8c6c9dfdd1c195b461b37e72d019
pchae <20016424+pchae@users.noreply.github.com>
2021-05-17 14:08:13 -0400

    Upgrade postcss version

---
+++ src/views/fintech.vue
@@ -0,0 +1,132 @@
+<template>
+  <div>
+
+    <Nav
+      id="Nav"
+      tColor="bk"
+      bgColor="b2"
+      v-if="!mobileView"
+    />
+
+    <NavMobile
+      id="NavMobile"
+      tColor="bk"
+      bgColor="b2"
+      v-if="mobileView"
+    />
+
+    <ArticleHeader
+      id="headline"
+      class="pt120 b2"
+      title="Redesigning Canada's Top 5 Financial institutions"
+      subhead=""
+    />
+
+    <ArticleDetails
+      id="detail"
+      class="pad2 b2"
+    />
+
+
+    <ImageBlock
+      id="header"
+      class="pb5 b2"
+      img-src="project/fintech/wallet.jpg"
+      img-alt="Header image"
+    />
+
+    <textblock
+      id="intro"
+      class="pad2 b2"
+      intro="Overview"
+      desc="Redesigns and conceptual design explorations for Royal Bank of Canada and Bank of Montreal's digital wallet apps and various digital channels."
+    />
+
+    <MediaBlock
+      id="RENAME"
+      class="pb5 b2"
+      subline=""
+      img-src="project/fintech/rewards.jpg"
+      img-alt="RENAME"
+    />
+
+    <MediaBlock
+      id="RENAME"
+      class="pb5 b2"
+      subline=""
+      img-src="project/fintech/web.jpg"
+      img-alt="responsive web layouts."
+    />
+
+    <Contact id="contact" class="b2" />
+
+    <Footer id="footer" class="b2" />
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+import Nav from "@/components/navigation/nav.vue";
+import NavMobile from "@/components/navigation/nav_mobile.vue";
+import ArticleHeader from "@/components/layouts/article_header.vue";
+import ArticleDetails from "@/components/layouts/article_details.vue";
+import ImageBlock from "@/components/layouts/image_block.vue";
+import MediaBlock from "@/components/layouts/MediaBlock.vue";
+import textblock from "@/components/layouts/article_text_block.vue";
+import Contact from "@/components/navigation/contact.vue";
+import Footer from "@/components/navigation/footer.vue";
+
+export default {
+  data: () => {
+    return {
+      mobileView: false
+    };
+  },
+  methods: {
+    handleView() {
+      this.mobileView = window.innerWidth <= 800;
+    },
+  },
+
+  name: "mma",
+  components: {
+    Nav,
+    NavMobile,
+    ArticleHeader,
+    ArticleDetails,
+    ImageBlock,
+    MediaBlock,
+    textblock,
+    Contact,
+    Footer,
+  },
+  metaInfo: {
+    // if no subcomponents specify a metaInfo.title, this title will be used
+    title: "Phil Choi",
+    // all titles will be injected into this template
+    titleTemplate: "%s - Fintech",
+  },
+  provide() {
+    return {
+      workInfo: {
+        client: "Royal Bank of Canada, Bank of Montreal",
+        year: "2016",
+        role: "Product Design Lead",
+        result: [
+          "Mixed"
+        ]
+      }
+    };
+  },
+
+  created() {
+    this.handleView();
+    window.addEventListener('resize', this.handleView);
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style></style>
