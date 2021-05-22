4d9bc67c9a64014aece95215cfc810f055ab0d55
pchae <20016424+pchae@users.noreply.github.com>
2021-05-22 10:48:37 -0400

    init
    - video container

---
+++ src/components/layouts/video_block.vue
@@ -0,0 +1,57 @@
+<template>
+  <div
+    :id="[id]"
+    :class="[padding, bgColor]">
+    <div class="container-lg media">
+      <video
+        ref="video"
+        autoplay
+        loop
+        muted
+        playsinline
+        :preload="preload"
+        :src="require('@/assets/' + vidSrc)"
+        :alt="vidAlt"
+        type="video/mp4"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Video',
+  props: {
+    vidSrc: {
+      type: String,
+      default: '',
+    },
+    vidAlt: {
+      type: String,
+      default: '',
+    },
+  },
+  data() {
+    return {
+      padding: '',
+      bgColor: '',
+      id: '',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.subtext {
+  font-weight: 400;
+  color: #181818;
+}
+
+.media img {
+  display: block;
+  width: 100%;
+  margin: 0;
+}
+
+</style>
