df22a8db2f8cbcf4fc7af1b1b2ca69c0e2eface9
pchae <20016424+pchae@users.noreply.github.com>
2021-05-26 12:12:29 -0400

    Add @vueuse/head
    - meta tag plugin

--- src/main.js
+++ src/main.js
@@ -3,13 +3,16 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import { createHead } from '@vueuse/head'
 import VueLazyLoad from 'vue3-lazyload';
 import '@/assets/css/layout.css';
 
+const head = createHead()
 
 createApp(App)
   .use(router)
   .use(store)
+  .use(head)
   .use(VueLazyLoad, {
     // options...
   })
