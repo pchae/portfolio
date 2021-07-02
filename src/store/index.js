cc2833d4f01ee56ebf8f87346f3b56aaa76cd072
pchae <20016424+pchae@users.noreply.github.com>
2021-07-01 22:03:52 -0400

    add projects.json to state

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore } from 'vuex';
 import aboutJson from '@/json/about.json';
+import projectJson from '@/json/projects.json';
 
 export default createStore({
   state: {
     about: aboutJson,
+    projects: projectJson,
   },
 
   getters: {
