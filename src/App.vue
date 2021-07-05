6c24c9e75c7e50e45ef9cd9172a3e82e4de0697c
pchae <20016424+pchae@users.noreply.github.com>
2021-07-05 03:39:18 -0400

    clean up formatting

--- src/App.vue
+++ src/App.vue
@@ -11,24 +11,21 @@
   </div>
 </template>
 
-<script>
-
-</script>
+<script></script>
 
 <style>
-  .fade-enter-active,
-  .fade-leave-active {
-    transition: opacity 0.5s ease;
-  }
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
 
-  .fade-enter-from,
-  .fade-leave-to {
-    opacity: 0;
-  }
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
 
-  .fade-enter-to,
-  .fade-leave-from {
-    opacity: 1;
-  }
-
+.fade-enter-to,
+.fade-leave-from {
+  opacity: 1;
+}
 </style>
